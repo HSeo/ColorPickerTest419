a74a4d5fd98d2678b3af8c5ad4785aeb5a8dcb5a
HirofumiSeo <seo@sciement.com>
2019-02-01 18:40:01 +0900

    Use RebuildWidget() for initializing the targeting widget. No error!

--- Source/ColorPickerTest419/NativeWidgetHostColorPicker.cpp
+++ Source/ColorPickerTest419/NativeWidgetHostColorPicker.cpp
@@ -1,8 +1,16 @@
 #include "NativeWidgetHostColorPicker.h"
 #include "SColorPicker.h"
 
-UNativeWidgetHostColorPicker::UNativeWidgetHostColorPicker(const FObjectInitializer& ObjectInitializer) : UNativeWidgetHost(ObjectInitializer) {
-  TSharedRef<SColorPicker> color_picker = SNew(SColorPicker);
+UNativeWidgetHostColorPicker::UNativeWidgetHostColorPicker(const FObjectInitializer& ObjectInitializer) : UNativeWidgetHost(ObjectInitializer) {}
 
-  SetContent(color_picker);
+void UNativeWidgetHostColorPicker::ReleaseSlateResources(bool bReleaseChildren) {
+  Super::ReleaseSlateResources(bReleaseChildren);
+  color_picker_.Reset();
+}
+
+TSharedRef<SWidget> UNativeWidgetHostColorPicker::RebuildWidget() {
+  SAssignNew(color_picker_, SColorPicker);
+  SetContent(color_picker_.ToSharedRef()); // Unnecessary??
+
+  return color_picker_.ToSharedRef();
 }
--- Source/ColorPickerTest419/NativeWidgetHostColorPicker.h
+++ Source/ColorPickerTest419/NativeWidgetHostColorPicker.h
@@ -4,6 +4,8 @@
 #include "Components/NativeWidgetHost.h"
 #include "NativeWidgetHostColorPicker.generated.h"
 
+class SColorPicker;
+
 UCLASS()
 class COLORPICKERTEST419_API UNativeWidgetHostColorPicker : public UNativeWidgetHost
 {
@@ -11,4 +13,11 @@ class COLORPICKERTEST419_API UNativeWidgetHostColorPicker : public UNativeWidget
 
 public:
   UNativeWidgetHostColorPicker(const FObjectInitializer& ObjectInitializer);
+  virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+
+protected:
+  virtual TSharedRef<SWidget> RebuildWidget() override;
+
+protected:
+  TSharedPtr<SColorPicker> color_picker_;
 };
